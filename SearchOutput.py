94e86105229f15d463c00a3692a6aabcf06c7a53
kwehen <110314567+kwehen@users.noreply.github.com>
2023-05-31 17:19:04 +0000

    Update SearchOutput.py
    Cleanup. Added output-fils_path variable

--- SearchOutput.py
+++ SearchOutput.py
@@ -1,6 +1,7 @@
 # If the output file has a hash that comes back with malicious greater than 0 it will be output to the screen
 file_path = "results.txt"
 search_string = "malicious=0"
+output_file_path = "malicious_results.txt"
 
 with open(file_path, "r") as file:
     with open(output_file_path, "w") as output_file:
